a4f627f482a979986189ce816ea59eabc730795b
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-04-10 01:27:26 +0600

    use context added

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NoDataMassage } from '../Layout/Layout';
 
-const Cart = ({cart, handleRemove}) => {
+const Cart = ({ cart, handleRemove }) => {
+    const msg = useContext(NoDataMassage)
+    
     return (
         <div className='bg-gray-200 px-4 rounded'>
             <h1 className='text-center font-bold text-xl py-2'>Order Summary : {cart.length} </h1>
             {
-                cart.map(item=> {
+               cart.length===0 && <p className='text-center text-gray-400 font-bold'><small>{msg}</small></p>
+            }
+
+            {
+                cart.map(item => {
                     return (
+
                         <div key={item._id} className='flex justify-between mt-2'>
+
+
                             <p>{item.name}</p>
-                            <button onClick={()=>handleRemove(item._id)} className='rounded bg-red-700 text-white py-1 px-2'>Remove</button>
+                            <button onClick={() => handleRemove(item._id)} className='rounded bg-red-700 text-white py-1 px-2'>Remove</button>
                         </div>
                     )
-                } )
+                })
             }
         </div>
     );
--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
+export const NoDataMassage = createContext('massage')
 const Layout = () => {
+    const massage = 'Buy some T-shirt'
     return (
         <div>
-            <Header />
-            <Outlet />
-            <ToastContainer />
+            <NoDataMassage.Provider value={massage}>
+                <Header />
+                <Outlet />
+                <ToastContainer />
+            </NoDataMassage.Provider>
         </div>
     );
 };
